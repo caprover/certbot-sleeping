0843d1cfecd83c48de97497dcd87f0c985ae84f7
Kasra Bigdeli <kasra85@gmail.com>
2020-08-23 22:54:34 -0400

    Fixed arch

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
-FROM certbot/certbot:${CERTBOT_ARCH}-${CERTBOT_VERSION}
+FROM certbot/certbot:${ORIGINAL_TAG}-${CERTBOT_VERSION}
 ENTRYPOINT ['/bin/sh','-c' ]
-CMD ['sleep 9999d']
+CMD ['sleep 1999d']
--- build_and_push.sh
+++ build_and_push.sh
@@ -8,7 +8,7 @@ set -e
 
 rm -f DockerfileProcessed.*
 echo "Cleaning up..."
-sleep 2s
+sleep 1s
 
 CERTBOT_IMAGE_NAME="caprover/certbot-sleeping"
 CERTBOT_VERSION="v1.6.0"
@@ -20,27 +20,29 @@ docker buildx ls
 docker buildx create --name mybuilder || echo "Already created!"
 docker buildx use mybuilder
 
-
-ARCHS="amd64 arm64v8 arm32v6"
+declare -a ARCH_CERTBOT_TAGS=("amd64" "arm64v8" "arm32v6")
+declare -a CERTBOT_ARCHS=("amd64" "arm64" "arm")
 
 echo ""
 echo "Building..."
-# Iterate the string variable using for loop
-for CERTBOT_ARCH in $ARCHS; do
-    echo $CERTBOT_ARCH
+for i in {0..2}
+do
+    ORIGINAL_TAG="${ARCH_CERTBOT_TAGS[$i]}"
+    CERTBOT_ARCH="${CERTBOT_ARCHS[$i]}"
+    echo "$CERTBOT_ARCH maps to cerbot:$ORIGINAL_TAG"
     eval "echo \"$(cat Dockerfile)\"" > DockerfileProcessed.$CERTBOT_ARCH
     docker buildx build --platform linux/$CERTBOT_ARCH -t $CERTBOT_IMAGE_NAME:$CERTBOT_ARCH-$CERTBOT_VERSION --push -f DockerfileProcessed.$CERTBOT_ARCH .
 done
 
 docker manifest create $CERTBOT_IMAGE_NAME:$CERTBOT_VERSION \
                        $CERTBOT_IMAGE_NAME:amd64-$CERTBOT_VERSION \
-                       $CERTBOT_IMAGE_NAME:arm64v8-$CERTBOT_VERSION \
-                       $CERTBOT_IMAGE_NAME:arm32v6-$CERTBOT_VERSION
+                       $CERTBOT_IMAGE_NAME:arm64-$CERTBOT_VERSION \
+                       $CERTBOT_IMAGE_NAME:arm-$CERTBOT_VERSION
 
 docker manifest create $CERTBOT_IMAGE_NAME:latest \
                        $CERTBOT_IMAGE_NAME:amd64-$CERTBOT_VERSION \
-                       $CERTBOT_IMAGE_NAME:arm64v8-$CERTBOT_VERSION \
-                       $CERTBOT_IMAGE_NAME:arm32v6-$CERTBOT_VERSION
+                       $CERTBOT_IMAGE_NAME:arm64-$CERTBOT_VERSION \
+                       $CERTBOT_IMAGE_NAME:arm-$CERTBOT_VERSION
 
 
 docker manifest push $CERTBOT_IMAGE_NAME:$CERTBOT_VERSION
